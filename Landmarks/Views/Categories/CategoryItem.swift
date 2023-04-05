b1056028c95007693b316660cc3e73f8b4a63edd
Yujin Lee <yj587878@gmail.com>
2023-04-05 19:52:24 +0900

    Composing Complex Interfaces

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by 이유진 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155,height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading,15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
